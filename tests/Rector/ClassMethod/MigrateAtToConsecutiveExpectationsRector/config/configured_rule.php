01468fa764958c62e7517a5c82ab170a62c1f479
TomasVotruba <tomas.vot@gmail.com>
2021-03-19 02:31:43 +0100

    improve tests

--- tests/Rector/ClassMethod/MigrateAtToConsecutiveExpectationsRector/config/configured_rule.php
+++ tests/Rector/ClassMethod/MigrateAtToConsecutiveExpectationsRector/config/configured_rule.php
@@ -6,6 +6,8 @@ use Rector\PHPUnit\Rector\ClassMethod\MigrateAtToConsecutiveExpectationsRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../../../config/config.php');
+
     $services = $containerConfigurator->services();
 
     $services->set(MigrateAtToConsecutiveExpectationsRector::class);
--- tests/Rector/Class_/ArrayArgumentToDataProviderRector/config/configured_rule.php
+++ tests/Rector/Class_/ArrayArgumentToDataProviderRector/config/configured_rule.php
@@ -6,6 +6,8 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../../../config/config.php');
+
     $services = $containerConfigurator->services();
 
     $services->set(ArrayArgumentToDataProviderRector::class)
