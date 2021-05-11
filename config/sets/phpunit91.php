29cf0de1cde62561dcd7946a83aad349225ede87
Joshua License <joshua.license@dvsa.gov.uk>
2021-05-11 21:51:56 +0100

    Fix CS

--- config/sets/phpunit91.php
+++ config/sets/phpunit91.php
@@ -19,17 +19,33 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                 // https://github.com/sebastianbergmann/phpunit/issues/4087
                 new MethodCallRename('PHPUnit\Framework\Assert', 'assertRegExp', 'assertMatchesRegularExpression'),
                 // https://github.com/sebastianbergmann/phpunit/issues/4090
-                new MethodCallRename('PHPUnit\Framework\Assert', 'assertNotRegExp', 'assertDoesNotMatchRegularExpression'),
+                new MethodCallRename(
+                    'PHPUnit\Framework\Assert',
+                    'assertNotRegExp',
+                    'assertDoesNotMatchRegularExpression'
+                ),
                 // https://github.com/sebastianbergmann/phpunit/issues/4078
                 new MethodCallRename('PHPUnit\Framework\Assert', 'assertFileNotExists', 'assertFileDoesNotExist'),
                 // https://github.com/sebastianbergmann/phpunit/issues/4081
                 new MethodCallRename('PHPUnit\Framework\Assert', 'assertFileNotIsReadable', 'assertFileIsNotReadable'),
                 // https://github.com/sebastianbergmann/phpunit/issues/4072
-                new MethodCallRename('PHPUnit\Framework\Assert', 'assertDirectoryNotIsReadable', 'assertDirectoryIsNotReadable'),
+                new MethodCallRename(
+                    'PHPUnit\Framework\Assert',
+                    'assertDirectoryNotIsReadable',
+                    'assertDirectoryIsNotReadable'
+                ),
                 // https://github.com/sebastianbergmann/phpunit/issues/4075
-                new MethodCallRename('PHPUnit\Framework\Assert', 'assertDirectoryNotIsWritable', 'assertDirectoryIsNotWritable'),
+                new MethodCallRename(
+                    'PHPUnit\Framework\Assert',
+                    'assertDirectoryNotIsWritable',
+                    'assertDirectoryIsNotWritable'
+                ),
                 // https://github.com/sebastianbergmann/phpunit/issues/4069
-                new MethodCallRename('PHPUnit\Framework\Assert', 'assertDirectoryNotExists', 'assertDirectoryDoesNotExist'),
+                new MethodCallRename(
+                    'PHPUnit\Framework\Assert',
+                    'assertDirectoryNotExists',
+                    'assertDirectoryDoesNotExist'
+                ),
                 // https://github.com/sebastianbergmann/phpunit/issues/4066
                 new MethodCallRename('PHPUnit\Framework\Assert', 'assertNotIsWritable', 'assertIsNotWritable'),
                 // https://github.com/sebastianbergmann/phpunit/issues/4063
