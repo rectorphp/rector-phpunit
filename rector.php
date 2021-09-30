93e37e18b97fa3ba9df8c359874354ca97a6df36
Tomas Votruba <tomas.vot@gmail.com>
2021-09-30 15:51:37 +0200

    setup run

--- rector.php
+++ rector.php
@@ -2,13 +2,27 @@
 
 declare(strict_types=1);
 
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
+    $parameters = $containerConfigurator->parameters();
 
-    $services->set(TypedPropertyRector::class);
-    $services->set(ClassPropertyAssignToConstructorPromotionRector::class);
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+    $parameters->set(Option::SKIP, [
+        // for tests
+        '*/Source/*',
+        '*/Fixture/*',
+    ]);
+
+    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(SetList::PHP_74);
+    $containerConfigurator->import(SetList::PHP_73);
+    $containerConfigurator->import(SetList::DEAD_CODE);
 };
+
