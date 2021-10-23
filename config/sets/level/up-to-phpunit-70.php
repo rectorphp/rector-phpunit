a7e84bc72e146570a79199c0bd5a1913609f66d8
Tomas Votruba <tomas.vot@gmail.com>
2021-10-23 15:45:54 +0200

    Add PHPUnitLevelSetList (#25)

---
+++ config/sets/level/up-to-phpunit-70.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_70);
+    $containerConfigurator->import(PHPUnitLevelSetList::UP_TO_PHPUNIT_60);
+};
