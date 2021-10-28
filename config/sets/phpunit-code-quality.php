35c36cdc3a724cf6ec7086a5b89dcce54a7f7f89
Tomas Votruba <tomas.vot@gmail.com>
2021-10-28 16:53:24 +0200

    configured ReturnArrayClassMethodToYieldRector

--- config/sets/phpunit-code-quality.php
+++ config/sets/phpunit-code-quality.php
@@ -3,17 +3,27 @@
 declare(strict_types=1);
 
 use Rector\CodingStyle\Rector\ClassMethod\ReturnArrayClassMethodToYieldRector;
+use Rector\CodingStyle\ValueObject\ReturnArrayClassMethodToYield;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Rector\Class_\ConstructClassMethodToSetUpTestCaseRector;
 use Rector\PHPUnit\Rector\MethodCall\AssertSameTrueFalseToAssertTrueFalseRector;
 use Rector\PHPUnit\Rector\MethodCall\RemoveExpectAnyFromMockRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(RemoveExpectAnyFromMockRector::class);
     $services->set(AddSeeTestAnnotationRector::class);
-    $services->set(ReturnArrayClassMethodToYieldRector::class);
+
+    $services->set(ReturnArrayClassMethodToYieldRector::class)
+        ->call('configure', [[
+            ReturnArrayClassMethodToYieldRector::METHODS_TO_YIELDS => ValueObjectInliner::inline([
+                new ReturnArrayClassMethodToYield('PHPUnit\Framework\TestCase', 'provide*'),
+                new ReturnArrayClassMethodToYield('PHPUnit\Framework\TestCase', 'dataProvider*'),
+            ]),
+        ]]);
+
     $services->set(ConstructClassMethodToSetUpTestCaseRector::class);
     $services->set(AssertSameTrueFalseToAssertTrueFalseRector::class);
 };
