f3259e09c668d16b82b833cf02f71f22bcfa2f08
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-11-10 07:52:39 +0700

    phpstan

--- src/PhpDoc/PhpDocValueToNodeMapper.php
+++ src/PhpDoc/PhpDocValueToNodeMapper.php
@@ -7,7 +7,6 @@ namespace Rector\PHPUnit\PhpDoc;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Scalar\String_;
 use PHPStan\PhpDocParser\Ast\PhpDoc\GenericTagValueNode;
-use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
 use PHPStan\Reflection\ReflectionProvider;
 use Rector\Core\PhpParser\Node\NodeFactory;
 
@@ -19,17 +18,8 @@ final class PhpDocValueToNodeMapper
     ) {
     }
 
-    public function mapGenericTagValueNode(PhpDocTagNode $phpDocTagNode): Expr
+    public function mapGenericTagValueNode(GenericTagValueNode $genericTagValueNode): Expr
     {
-        $genericTagValueNode = $phpDocTagNode->value;
-        if (! $genericTagValueNode instanceof GenericTagValueNode) {
-            return new String_($genericTagValueNode->value);
-        }
-
-        if ($phpDocTagNode->name === '@expectedExceptionMessage') {
-            return new String_($genericTagValueNode->value);
-        }
-
         if (\str_contains($genericTagValueNode->value, '::')) {
             [$class, $constant] = explode('::', $genericTagValueNode->value);
             return $this->nodeFactory->createShortClassConstFetch($class, $constant);
