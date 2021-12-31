91b45854b573f9397d6e20be02906e79ccc24373
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-31 19:11:47 +0700

    Skip Inner class __constuct on ConstructClassMethodToSetUpTestCaseRector

---
+++ tests/Rector/Class_/ConstructClassMethodToSetUpTestCaseRector/InnerClassTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Tests\Rector\Class_\ConstructClassMethodToSetUpTestCaseRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class InnerClassTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return Iterator<SmartFileInfo>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/FixtureInnerClass');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/inner_class_configured_rule.php';
+    }
+}
