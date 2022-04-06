dc49eb6d382857e9a5917a86e040adc8896c1083
Jiří Bok <jiri.bok@protonmail.com>
2022-04-06 13:23:46 +0200

    [Feature] Another attempt to fix failing easy-ci check-active-class check

--- easy-ci.php
+++ easy-ci.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Contract\Rector\RectorInterface;
+use Rector\PHPUnit\Naming\TestClassNameResolverInterface;
+use Rector\Set\Contract\SetListInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCI\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::TYPES_TO_SKIP, [
-        \Rector\PHPUnit\Naming\TestClassNameResolver::class,
+        TestClassNameResolverInterface::class,
+        RectorInterface::class,
+        SetListInterface::class,
     ]);
 };
