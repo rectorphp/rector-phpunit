471e026d8ed80af29c41514c4f71e34ae47cdd03
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-12 22:06:20 +0700

    [DX] Use RectorConfig in tests (#59)

--- tests/Rector/Class_/ArrayArgumentToDataProviderRector/config/configured_rule.php
+++ tests/Rector/Class_/ArrayArgumentToDataProviderRector/config/configured_rule.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
+use Rector\Config\RectorConfig;
+
 use Rector\PHPUnit\Rector\Class_\ArrayArgumentToDataProviderRector;
 use Rector\PHPUnit\ValueObject\ArrayArgumentToDataProvider;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (RectorConfig $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../../config/config.php');
 
     $services = $containerConfigurator->services();
