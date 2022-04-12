071f8d960485ca556db5e7981df8d14b94648d44
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-12 21:32:03 +0700

    rectify locally

--- config/config.php
+++ config/config.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-
 return static function (RectorConfig $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
