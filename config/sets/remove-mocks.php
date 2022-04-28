f43ac1741b7f416cb14cd36e61f7401418972fbc
Tomas Votruba <tomas.vot@gmail.com>
2022-04-29 00:03:10 +0200

    add create mock to remove mocks set

--- config/sets/remove-mocks.php
+++ config/sets/remove-mocks.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Rector\MethodCall\GetMockBuilderGetMockToCreateMockRector;
 use Rector\PHPUnit\Rector\MethodCall\RemoveSetMethodsMethodCallRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(RemoveSetMethodsMethodCallRector::class);
+    $rectorConfig->rule(GetMockBuilderGetMockToCreateMockRector::class);
 };
