81f779af6098414a559ec94f73563ff1a009d4b5
Tomas Votruba <tomas.vot@gmail.com>
2022-07-04 00:02:54 +0200

    cs

--- ecs.php
+++ ecs.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -17,9 +16,7 @@ return static function (ECSConfig $ecsConfig): void {
         __DIR__ . '/rector.php',
     ]);
 
-    $ecsConfig->skip([
-        '*/Source/*', '*/Fixture/*',
-    ]);
+    $ecsConfig->skip(['*/Source/*', '*/Fixture/*']);
 
     $ecsConfig->lineEnding("\n");
 };
