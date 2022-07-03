ca42a8ea43abe8e975382c585ec9adbf293df01f
Tomas Votruba <tomas.vot@gmail.com>
2022-07-03 21:13:24 +0200

    make use of RectorConfig (#91)

--- rector.php
+++ rector.php
@@ -3,19 +3,18 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $parameters = $rectorConfig->parameters();
+    $rectorConfig->importNames();
 
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::PARALLEL, true);
+    $rectorConfig->parallel();
 
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+
+    $rectorConfig->skip([
         // for tests
         '*/Source/*',
         '*/Fixture/*',
@@ -31,15 +30,16 @@ return static function (RectorConfig $rectorConfig): void {
         ],
     ]);
 
-    // needed for DEAD_CODE list, just in split package like this
-    $rectorConfig->import(__DIR__ . '/config/config.php');
-
-    $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
-    $rectorConfig->import(SetList::DEAD_CODE);
-    $rectorConfig->import(SetList::CODE_QUALITY);
-    $rectorConfig->import(SetList::CODING_STYLE);
-    $rectorConfig->import(SetList::EARLY_RETURN);
-    $rectorConfig->import(SetList::NAMING);
+    $rectorConfig->sets([
+        // needed for DEAD_CODE list, just in split package like this
+        __DIR__ . '/config/config.php',
+        LevelSetList::UP_TO_PHP_81,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::EARLY_RETURN,
+        SetList::NAMING,
+    ]);
 
     $rectorConfig->ruleWithConfiguration(StringClassNameToClassConstantRector::class, [
         // keep unprefixed to protected from downgrade
