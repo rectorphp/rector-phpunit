78351593db0c8a4373454c3dbf40a6ce660aa990
Tomas Votruba <tomas.vot@gmail.com>
2022-09-01 10:32:18 +0200

    [tests] Use file paths over value objects in tests to improve performance

--- tests/Rector/Class_/ConstructClassMethodToSetUpTestCaseRector/InnerClassTest.php
+++ tests/Rector/Class_/ConstructClassMethodToSetUpTestCaseRector/InnerClassTest.php
@@ -13,9 +13,9 @@ final class InnerClassTest extends AbstractRectorTestCase
     /**
      * @dataProvider provideData()
      */
-    public function test(SmartFileInfo $fileInfo): void
+    public function test(string $filePath): void
     {
-        $this->doTestFileInfo($fileInfo);
+        $this->doTestFile($filePath);
     }
 
     /**
