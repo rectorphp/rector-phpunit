9e76347e4933f74bf74cad496a1183f8db8b1b1b
Tomas Votruba <tomas.vot@gmail.com>
2022-11-14 15:01:58 +0100

    trigger CI

--- rector.php
+++ rector.php
@@ -39,6 +39,7 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODING_STYLE,
         SetList::EARLY_RETURN,
         SetList::NAMING,
+        SetList::PRIVATIZATION,
     ]);
 
     $rectorConfig->ruleWithConfiguration(StringClassNameToClassConstantRector::class, [
