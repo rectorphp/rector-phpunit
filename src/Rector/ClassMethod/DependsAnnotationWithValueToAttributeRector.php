57eb6ffb911217b89f356a71f00af4d492970d82
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-10 17:13:10 +0700

    [PHPUnit] Add DependsAnnotationWithValueToAttributeRector

---
+++ src/Rector/ClassMethod/DependsAnnotationWithValueToAttributeRector.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Rector\ClassMethod;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class DependsAnnotationWithValueToAttributeRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Change depends annotations with value to attribute', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+use PHPUnit\Framework\TestCase;
+
+final class SomeTest extends TestCase
+{
+    public function testOne() {}
+    public function testTwo() {}
+
+    /**
+     * @depends testOne
+     * @depends testTwo
+     */
+    public function testThree(): void
+    {
+    }
+}
+CODE_SAMPLE
+
+                ,
+                <<<'CODE_SAMPLE'
+use PHPUnit\Framework\TestCase;
+
+final class SomeTest extends TestCase
+{
+    public function testOne() {}
+    public function testTwo() {}
+
+    /**
+     * @depends testOne
+     * @depends testTwo
+     */
+    public function testThree(): void
+    {
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    /**
+     * @param ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNode($node);
+        if (! $phpDocInfo instanceof PhpDocInfo) {
+            return null;
+        }
+
+        return $node;
+    }
+}
