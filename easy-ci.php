3e9d821bd20e5385038788d26f00b29eea59209a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-02 19:33:42 +0700

    Remove check-markdown command from composer fix-cs and composer docs (#145)
    * Remove check-markdown command from composer fix-cs and composer docs
    
    * add missing easy-ci config
    
    * fix

--- easy-ci.php
+++ easy-ci.php
@@ -2,13 +2,29 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Contract\Rector\RectorInterface;
-use Rector\Set\Contract\SetListInterface;
+use Rector\PHPUnit\Rector\Class_\ArrayArgumentToDataProviderRector;
+use Rector\PHPUnit\Rector\Class_\ProphecyPHPDocRector;
+use Rector\PHPUnit\Rector\ClassMethod\CreateMockToAnonymousClassRector;
+use Rector\PHPUnit\Rector\ClassMethod\RemoveEmptyTestMethodRector;
+use Rector\PHPUnit\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
+use Rector\PHPUnit\Rector\ClassMethod\TryCatchToExpectExceptionRector;
+use Rector\PHPUnit\Rector\MethodCall\AssertResourceToClosedResourceRector;
+use Rector\PHPUnit\Rector\MethodCall\CreateMockToCreateStubRector;
+use Rector\PHPUnit\Rector\MethodCall\UseSpecificWithMethodRector;
 use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
+    $easyCIConfig->paths([__DIR__ . '/config', __DIR__ . '/src']);
+
     $easyCIConfig->typesToSkip([
-        RectorInterface::class,
-        SetListInterface::class,
+        CreateMockToAnonymousClassRector::class,
+        RemoveEmptyTestMethodRector::class,
+        ReplaceTestAnnotationWithPrefixedFunctionRector::class,
+        TryCatchToExpectExceptionRector::class,
+        ArrayArgumentToDataProviderRector::class,
+        ProphecyPHPDocRector::class,
+        AssertResourceToClosedResourceRector::class,
+        CreateMockToCreateStubRector::class,
+        UseSpecificWithMethodRector::class,
     ]);
 };
