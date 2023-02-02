5e0babaccb636ce7fc1760fcae01549cc692cfca
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-02 17:14:36 +0700

    add missing easy-ci config

--- easy-ci.php
+++ easy-ci.php
@@ -2,13 +2,11 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Contract\Rector\RectorInterface;
-use Rector\Set\Contract\SetListInterface;
 use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
+    $easyCIConfig->paths([__DIR__ . '/config', __DIR__ . '/src']);
+
     $easyCIConfig->typesToSkip([
-        RectorInterface::class,
-        SetListInterface::class,
     ]);
 };
