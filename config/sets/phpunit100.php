dcfdf20afde70255c8621828d915747b65575949
Tomas Votruba <tomas.vot@gmail.com>
2023-02-06 18:30:45 +0000

    Add post class rename to RemoveTestSuffixFromAbstractTestClassesRector (#153)

--- config/sets/phpunit100.php
+++ config/sets/phpunit100.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Rector\Class_\StaticDataProviderClassMethodRector;
+use Rector\PHPUnit\Rector\ClassLike\RemoveTestSuffixFromAbstractTestClassesRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/annotations-to-attributes.php');
 
-    $rectorConfig->rules([StaticDataProviderClassMethodRector::class]);
+    $rectorConfig->rules([
+        StaticDataProviderClassMethodRector::class,
+        RemoveTestSuffixFromAbstractTestClassesRector::class,
+    ]);
 };
---
+++ tests/Rector/ClassLike/RemoveTestSuffixFromAbstractTestClassesRector/Fixture/extends_test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rector\PHPUnit\Tests\Rector\ClassLike\RemoveTestSuffixFromAbstractTestClassesRector\Fixture;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class ExtendsTest extends TestCase
+{
+}
+
+?>
