8b4f2f92bf0fd3cfe48f88e8730ccb2089bc98b1
Tomas Votruba <tomas.vot@gmail.com>
2023-02-06 17:56:11 +0000

    Add RemoveTestSuffixFromAbstractTestClassesRector (#151)
    Co-authored-by: johan <johan@adivare.nl>

---
+++ tests/Rector/ClassLike/RemoveTestSuffixFromAbstractTestClassesRector/Expected/ExtendsTestCase.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Rector\PHPUnit\Tests\Rector\ClassLike\RemoveTestSuffixFromAbstractTestClassesRector\Fixture;
+
+abstract class ExtendsTestCase extends \PHPUnit\Framework\TestCase
+{
+}
---
+++ tests/Rector/ClassLike/RemoveTestSuffixFromAbstractTestClassesRector/RemoveTestSuffixFromAbstractTestClassesRectorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Tests\Rector\ClassLike\RemoveTestSuffixFromAbstractTestClassesRector;
+
+use Iterator;
+use Nette\Utils\FileSystem;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class RemoveTestSuffixFromAbstractTestClassesRectorTest extends AbstractRectorTestCase
+{
+    public function testNoChanges(): void
+    {
+        $this->doTestFile(__DIR__ . '/Fixture/skip_abstract_class_without_suffix.php.inc');
+    }
+
+    public function testChanges(): void
+    {
+        $this->doTestFile(__DIR__ . '/Fixture/extends_test.php.inc');
+
+        $this->assertFileWasAdded(
+            __DIR__ . '/Fixture/ExtendsTestCase.php',
+            FileSystem::read(__DIR__ . '/Expected/ExtendsTestCase.php')
+        );
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
