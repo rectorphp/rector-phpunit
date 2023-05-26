9f00a628290d0a89a87003e33148ae61081b5f92
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-05-26 22:58:35 +0700

    [PHPUnit 10] Allow no namespace test on RemoveTestSuffixFromAbstractTestClassesRector and remove parent attribute (#174)
    * [PHPUnit 10] Allow no namespace test on RemoveTestSuffixFromAbstractTestClassesRector and remove parent attribute
    
    * [ci-review] Rector Rectify
    
    * Fix phpstan
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <actions@github.com>

--- src/Rector/ClassLike/RemoveTestSuffixFromAbstractTestClassesRector.php
+++ src/Rector/ClassLike/RemoveTestSuffixFromAbstractTestClassesRector.php
@@ -11,8 +11,8 @@ use PhpParser\Node\Stmt\Namespace_;
 use Rector\Core\Application\FileSystem\RemovedAndAddedFilesCollector;
 use Rector\Core\Configuration\RenamedClassesDataCollector;
 use Rector\Core\Exception\ShouldNotHappenException;
+use Rector\Core\PhpParser\Node\CustomNode\FileWithoutNamespace;
 use Rector\Core\Rector\AbstractRector;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
 use Rector\Symfony\Printer\NeighbourClassLikePrinter;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -23,12 +23,32 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class RemoveTestSuffixFromAbstractTestClassesRector extends AbstractRector
 {
+    private FileWithoutNamespace|Namespace_|null $rootNode = null;
+
     public function __construct(
         private readonly NeighbourClassLikePrinter $neighbourClassLikePrinter,
         private readonly TestsNodeAnalyzer $testsNodeAnalyzer,
         private readonly RenamedClassesDataCollector $renamedClassesDataCollector,
         private readonly RemovedAndAddedFilesCollector $removedAndAddedFilesCollector
     ) {
+    }
+
+    /**
+     * @return Node[]|null
+     */
+    public function beforeTraverse(array $nodes): ?array
+    {
+        // ensure reset early on every run to avoid reuse existing value
+        $this->rootNode = null;
+
+        foreach ($nodes as $node) {
+            if ($node instanceof FileWithoutNamespace || $node instanceof Namespace_) {
+                $this->rootNode = $node;
+                break;
+            }
+        }
+
+        return parent::beforeTraverse($nodes);
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -111,11 +131,10 @@ CODE_SAMPLE
     {
         $filePath = $this->file->getFilePath();
 
-        $parentNode = $class->getAttribute(AttributeKey::PARENT_NODE);
-        if (! $parentNode instanceof Namespace_) {
+        if (! $this->rootNode instanceof FileWithoutNamespace && ! $this->rootNode instanceof Namespace_) {
             throw new ShouldNotHappenException();
         }
 
-        $this->neighbourClassLikePrinter->printClassLike($class, $parentNode, $filePath, $this->file);
+        $this->neighbourClassLikePrinter->printClassLike($class, $this->rootNode, $filePath, $this->file);
     }
 }
---
+++ tests/Rector/ClassLike/RemoveTestSuffixFromAbstractTestClassesRector/Fixture/no_namespace_extends_test.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+abstract class NoNamespaceExtendsTest extends TestCase
+{
+}
+
+?>
