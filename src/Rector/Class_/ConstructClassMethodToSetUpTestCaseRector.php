84dc2e3c178f2d2571ffafd7abf47f4049698ca4
Tomas Votruba <tomas.vot@gmail.com>
2023-06-05 16:12:14 +0200

    Remove removeNode() from ConstructClassMethodToSetUpTestCaseRector (#186)

--- src/Rector/Class_/ConstructClassMethodToSetUpTestCaseRector.php
+++ src/Rector/Class_/ConstructClassMethodToSetUpTestCaseRector.php
@@ -15,6 +15,7 @@ use PhpParser\Node\Stmt\Expression;
 use Rector\Core\NodeAnalyzer\ClassAnalyzer;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\ValueObject\MethodName;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\PHPUnit\NodeAnalyzer\SetUpMethodDecorator;
 use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
 use Rector\Privatization\NodeManipulator\VisibilityManipulator;
@@ -115,7 +116,9 @@ CODE_SAMPLE
             $this->setUpMethodDecorator->decorate($constructClassMethod);
             $this->visibilityManipulator->makeProtected($constructClassMethod);
         } else {
-            $this->removeNode($constructClassMethod);
+            $stmtKey = $constructClassMethod->getAttribute(AttributeKey::STMT_KEY);
+            unset($node->stmts[$stmtKey]);
+
             $setUpClassMethod->stmts = array_merge((array) $setUpClassMethod->stmts, $addedStmts);
         }
 
