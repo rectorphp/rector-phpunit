8f78df1d25c21d6aab368400df73c8630d916202
Tomas Votruba <tomas.vot@gmail.com>
2023-06-12 12:08:30 +0200

    Remove CreateMockToCreateStubRector as unclear, not part of any set (#189)

--- easy-ci.php
+++ easy-ci.php
@@ -3,15 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Core\Contract\Rector\RectorInterface;
-use Rector\PHPUnit\Rector\Class_\ArrayArgumentToDataProviderRector;
-use Rector\PHPUnit\Rector\Class_\ProphecyPHPDocRector;
-use Rector\PHPUnit\Rector\ClassMethod\CreateMockToAnonymousClassRector;
-use Rector\PHPUnit\Rector\ClassMethod\RemoveEmptyTestMethodRector;
-use Rector\PHPUnit\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
-use Rector\PHPUnit\Rector\ClassMethod\TryCatchToExpectExceptionRector;
-use Rector\PHPUnit\Rector\MethodCall\AssertResourceToClosedResourceRector;
-use Rector\PHPUnit\Rector\MethodCall\CreateMockToCreateStubRector;
-use Rector\PHPUnit\Rector\MethodCall\UseSpecificWithMethodRector;
+use Rector\Set\Contract\SetListInterface;
 use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
@@ -19,6 +11,6 @@ return static function (EasyCIConfig $easyCIConfig): void {
 
     $easyCIConfig->typesToSkip([
         RectorInterface::class,
-        \Rector\Set\Contract\SetListInterface::class,
+        SetListInterface::class,
     ]);
 };
