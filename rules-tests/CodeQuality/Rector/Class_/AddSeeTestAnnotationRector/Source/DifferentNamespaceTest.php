cc1fd7213f78a1bfdbbf6fcb8267e5d99ae7ac84
Tomas Votruba <tomas.vot@gmail.com>
2023-07-12 17:59:57 +0200

    Move code quality to own category

---
+++ rules-tests/CodeQuality/Rector/Class_/AddSeeTestAnnotationRector/Source/DifferentNamespaceTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Tests\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector\Source;
+
+class DifferentNamespaceTest
+{
+
+}
--- rules-tests/PHPUnit100/Rector/Class_/AddProphecyTraitRector/config/configured_rule.php
+++ rules-tests/PHPUnit100/Rector/Class_/AddProphecyTraitRector/config/configured_rule.php
@@ -6,7 +6,7 @@ use Rector\Config\RectorConfig;
 use Rector\PHPUnit\PHPUnit100\Rector\Class_\AddProphecyTraitRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
 
     $rectorConfig->rule(AddProphecyTraitRector::class);
 };
