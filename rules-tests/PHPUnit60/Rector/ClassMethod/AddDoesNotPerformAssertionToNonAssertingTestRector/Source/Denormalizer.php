ff2e2c02dd32423ab990117d114779b02b2208c2
Tomas Votruba <tomas.vot@gmail.com>
2023-07-12 15:50:32 +0200

    [PHPUnit 6] Move AddDoesNotPerformAssertionToNonAssertingTestRector to own PHPUnit60 category

---
+++ rules-tests/PHPUnit60/Rector/ClassMethod/AddDoesNotPerformAssertionToNonAssertingTestRector/Source/Denormalizer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Tests\PHPUnit60\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector\Source;
+
+final class Denormalizer
+{
+    public function __construct(private DenormalizerInterface $denormalizer)
+    {
+    }
+
+    public function handle(array $data, string $type): ?array
+    {
+        try {
+            return $this->denormalizer->denormalize($data, $type);
+        } catch (\Throwable $throwable) {
+            return null;
+        }
+    }
+}
---
+++ rules-tests/PHPUnit60/Rector/ClassMethod/AddDoesNotPerformAssertionToNonAssertingTestRector/Source/SomeUser.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Tests\PHPUnit60\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector\Source;
+
+class SomeUser
+{
+
+}
