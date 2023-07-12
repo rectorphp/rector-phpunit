aebd6940d72983a1c1b30b75d67b9b24e279031c
Tomas Votruba <tomas.vot@gmail.com>
2023-07-12 16:58:01 +0200

    [PHPUnit 10] Move rules to own PHPUnit100 category (#224)

---
+++ rules-tests/PHPUnit100/Rector/Class_/StaticDataProviderClassMethodRector/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+use Rector\PHPUnit\PHPUnit100\Rector\Class_\StaticDataProviderClassMethodRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+
+    $rectorConfig->rule(StaticDataProviderClassMethodRector::class);
+};
--- tests/Rector/Class_/StaticDataProviderClassMethodRector/Source/SomeClassToMock.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Rector\PHPUnit\Tests\Rector\Class_\StaticDataProviderClassMethodRector\Source;
-
-class SomeClassToMock {}
