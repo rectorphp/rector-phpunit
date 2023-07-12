3468aa4ad701ac28e57848ac3e8558f1c82b3bd2
Tomas Votruba <tomas.vot@gmail.com>
2023-07-12 10:28:25 +0200

    [DX] Merge 9.1 set to PHPUnit 10, to keep it simple and major-only as all other sets

--- config/sets/level/up-to-phpunit-91.php
+++ config/sets/level/up-to-phpunit-91.php
@@ -3,10 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
-use Rector\PHPUnit\Set\PHPUnitSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(PHPUnitSetList::PHPUNIT_91);
-    $rectorConfig->import(PHPUnitLevelSetList::UP_TO_PHPUNIT_90);
 };
