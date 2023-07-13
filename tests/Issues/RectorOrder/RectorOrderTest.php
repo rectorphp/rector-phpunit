ef309ff307ea750f6b0d4f798cf1c43b58cebe38
Tomas Votruba <tomas.vot@gmail.com>
2023-07-13 12:18:28 +0200

    Move core RectorOrder test here (#227)

---
+++ tests/Issues/RectorOrder/RectorOrderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Tests\Issues\RectorOrder;
+
+use Iterator;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class RectorOrderTest extends AbstractRectorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
