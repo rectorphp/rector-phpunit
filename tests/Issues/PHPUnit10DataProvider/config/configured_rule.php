961cae62c44af1294ffb30de9bc77847590fe0f1
GitHub Action <actions@github.com>
2023-12-26 14:28:22 +0000

    [ci-review] Rector Rectify

--- tests/Issues/PHPUnit10DataProvider/config/configured_rule.php
+++ tests/Issues/PHPUnit10DataProvider/config/configured_rule.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        PHPUnitSetList::PHPUNIT_100,
-    ]);
+    $rectorConfig->sets([PHPUnitSetList::PHPUNIT_100]);
 };
