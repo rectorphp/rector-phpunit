1044c05363ac7ac98c8c9de23ec8bac8a9c5b647
Tomas Votruba <tomas.vot@gmail.com>
2024-06-03 23:47:18 +0900

    remove deprecated phpunit level set list and unused sets

--- config/sets/phpunit-exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-
-/** @deprecated Use PHPUnit 6.0 set instead, as related to the version */
-return static function (RectorConfig $rectorConfig): void {
-};
--- config/sets/remove-mocks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-
-/** @deprecated Use CodeQuality set instead, as related to code-quality */
-return static function (RectorConfig $rectorConfig): void {
-};
