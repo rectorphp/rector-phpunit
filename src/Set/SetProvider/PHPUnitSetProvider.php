62e0ace66ac31bcd59954c6131dd294ba6ecedb9
Tomas Votruba <tomas.vot@gmail.com>
2024-06-25 01:18:27 +0900

    kick off PHPUnit set provider

--- src/Set/SetProvider/PHPUnitSetProvider.php
+++ src/Set/SetProvider/PHPUnitSetProvider.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Rector\PHPUnit\Set\SetProvider;
 
 use Rector\Set\Contract\SetProviderInterface;
+use Rector\Set\Enum\SetGroup;
 use Rector\Set\ValueObject\ComposerTriggeredSet;
+use Rector\Set\ValueObject\Set;
 
 /**
  * @api collected in core
@@ -19,52 +21,60 @@ final class PHPUnitSetProvider implements SetProviderInterface
     {
         return [
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '4.0',
                 __DIR__ . '/../../../config/sets/phpunit40.php'
             ),
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '5.0',
                 __DIR__ . '/../../../config/sets/phpunit50.php'
             ),
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '6.0',
                 __DIR__ . '/../../../config/sets/phpunit60.php'
             ),
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '7.0',
                 __DIR__ . '/../../../config/sets/phpunit70.php'
             ),
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '8.0',
                 __DIR__ . '/../../../config/sets/phpunit80.php'
             ),
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '9.0',
                 __DIR__ . '/../../../config/sets/phpunit90.php'
             ),
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '10.0',
                 __DIR__ . '/../../../config/sets/phpunit100.php'
             ),
             new ComposerTriggeredSet(
-                'phpunit',
+                SetGroup::PHPUNIT,
                 'phpunit/phpunit',
                 '11.0',
                 __DIR__ . '/../../../config/sets/phpunit110.php'
+            ),
+
+            new Set(SetGroup::PHPUNIT, 'Code Quality', __DIR__ . '/../../../config/sets/phpunit-code-quality.php'),
+
+            new Set(
+                SetGroup::ATTRIBUTES,
+                'PHPUnit Attributes',
+                __DIR__ . '/../../../config/sets/annotations-to-attributes.php'
             ),
         ];
     }
