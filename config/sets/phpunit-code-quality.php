adb831d0d4eefcbd1aeec2523ee552851d0bdcd1
Tomas Votruba <tomas.vot@gmail.com>
2024-09-29 09:02:01 +0200

    [CodeQuality] Add SingleWithConsecutiveToWithRector (#370)
    * register rule to code quality set
    
    * [CodeQuality] Add SingleWithConsecutiveToWithRector

--- config/sets/phpunit-code-quality.php
+++ config/sets/phpunit-code-quality.php
@@ -26,7 +26,9 @@ use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertRegExpRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertSameBoolNullToSpecificMethodRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertSameTrueFalseToAssertTrueFalseRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertTrueFalseToSpecificMethodRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\NarrowIdenticalWithConsecutiveRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\RemoveExpectAnyFromMockRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\SingleWithConsecutiveToWithRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\UseSpecificWillMethodRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\UseSpecificWithMethodRector;
 
@@ -42,6 +44,10 @@ return static function (RectorConfig $rectorConfig): void {
         TestWithToDataProviderRector::class,
         AssertEqualsOrAssertSameFloatParameterToSpecificMethodsTypeRector::class,
         DataProviderArrayItemsNewLinedRector::class,
+
+        // narrow with consecutive
+        NarrowIdenticalWithConsecutiveRector::class,
+        SingleWithConsecutiveToWithRector::class,
 
         // specific asserts
         AssertCompareOnCountableWithMethodToAssertCountRector::class,
