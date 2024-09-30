3e592ec6ce25d67c26faf78c91a8ac11fb310d25
Tomas Votruba <tomas.vot@gmail.com>
2024-09-30 10:22:49 +0200

    Add nested callback assert (#378)

--- src/NodeFactory/ConsecutiveIfsFactory.php
+++ src/NodeFactory/ConsecutiveIfsFactory.php
@@ -8,7 +8,9 @@ use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\BinaryOp\Identical;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Identifier;
@@ -17,12 +19,14 @@ use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\If_;
 use Rector\Exception\NotImplementedYetException;
 use Rector\Exception\ShouldNotHappenException;
+use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\PHPUnit\Enum\ConsecutiveVariable;
 
 final readonly class ConsecutiveIfsFactory
 {
     public function __construct(
-        private MatcherInvocationCountMethodCallNodeFactory $matcherInvocationCountMethodCallNodeFactory
+        private MatcherInvocationCountMethodCallNodeFactory $matcherInvocationCountMethodCallNodeFactory,
+        private NodeNameResolver $nodeNameResolver,
     ) {
     }
 
@@ -52,10 +56,21 @@ final readonly class ConsecutiveIfsFactory
                 }
 
                 $assertMethodCall = $assertArrayItem->value;
-                $assertMethodCall->name = $this->createAssertMethodName($assertMethodCall);
-                $assertMethodCall->args[] = new Arg(new ArrayDimFetch($parametersVariable, new LNumber($assertKey)));
 
-                $ifStmts[] = new Expression($assertMethodCall);
+                if ($this->nodeNameResolver->isName($assertMethodCall->name, 'equalTo')) {
+                    $assertMethodCallExpression = $this->createAssertMethodCall(
+                        $assertMethodCall,
+                        $parametersVariable,
+                        $assertKey
+                    );
+                    $ifStmts[] = $assertMethodCallExpression;
+                } elseif ($this->nodeNameResolver->isName($assertMethodCall->name, 'callback')) {
+                    $ifStmts[] = $this->createClosureAssignExpression($assertMethodCall);
+                    $ifStmts[] = $this->createAssertClosureExpression($parametersVariable, $assertKey);
+                } else {
+                    $methodName = $this->nodeNameResolver->getName($assertMethodCall->name);
+                    throw new NotImplementedYetException($methodName ?? 'dynamic name');
+                }
             }
 
             $ifs[] = new If_(new Identical($matcherMethodCall, new LNumber($key + 1)), [
@@ -66,16 +81,39 @@ final readonly class ConsecutiveIfsFactory
         return $ifs;
     }
 
-    private function createAssertMethodName(MethodCall $assertMethodCall): Identifier
+    private function createAssertMethodCall(
+        MethodCall $assertMethodCall,
+        Variable $parametersVariable,
+        int $parameterPositionKey
+    ): Expression {
+        $assertMethodCall->name = new Identifier('assertEquals');
+        $parametersArrayDimFetch = new ArrayDimFetch($parametersVariable, new LNumber($parameterPositionKey));
+
+        $assertMethodCall->args[] = new Arg($parametersArrayDimFetch);
+
+        return new Expression($assertMethodCall);
+    }
+
+    private function createClosureAssignExpression(MethodCall $assertMethodCall): Expression
     {
-        if (! $assertMethodCall->name instanceof Identifier) {
-            throw new ShouldNotHappenException();
-        }
+        $callableFirstArg = $assertMethodCall->getArgs()[0];
 
-        if ($assertMethodCall->name->toString() === 'equalTo') {
-            return new Identifier('assertEquals');
-        }
+        $callbackVariable = new Variable('callback');
+        $callbackAssign = new Assign($callbackVariable, $callableFirstArg->value);
 
-        throw new NotImplementedYetException($assertMethodCall->name->toString());
+        return new Expression($callbackAssign);
+    }
+
+    private function createAssertClosureExpression(Variable $parametersVariable, int $parameterPositionKey): Expression
+    {
+        $callbackVariable = new Variable('callback');
+        $parametersArrayDimFetch = new ArrayDimFetch($parametersVariable, new LNumber($parameterPositionKey));
+
+        $callbackFuncCall = new FuncCall($callbackVariable, [new Arg($parametersArrayDimFetch)]);
+
+        // add assert true to the callback
+        $assertTrueMethodCall = new MethodCall(new Variable('this'), 'assertTrue', [new Arg($callbackFuncCall)]);
+
+        return new Expression($assertTrueMethodCall);
     }
 }
