a5265a86d96a9ba7d7f9dbc20a19a3a665db70f2
Tomas Votruba <tomas.vot@gmail.com>
2024-11-04 18:53:42 +0100

    Add enum/const fetch support to AssertEqualsToSameRector (#394)

---
+++ rules-tests/CodeQuality/Rector/MethodCall/AssertEqualsToSameRector/Source/SimpleConstantList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\PHPUnit\Tests\CodeQuality\Rector\MethodCall\AssertEqualsToSameRector\Source;
+
+final class SimpleConstantList
+{
+    public const TOP = 'top';
+
+    public const BOTTOM = 'bottom';
+}
