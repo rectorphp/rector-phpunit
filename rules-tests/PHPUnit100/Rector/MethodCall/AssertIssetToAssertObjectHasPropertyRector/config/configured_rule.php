1e74358f5fbaf95ab19d4d801f1667b6d6fdbc9d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-11-10 14:47:14 +0700

    [PHPUnit 10] Remove AssertIssetToAssertObjectHasPropertyRector (#405)

--- rules-tests/PHPUnit100/Rector/MethodCall/AssertIssetToAssertObjectHasPropertyRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\PHPUnit\PHPUnit100\Rector\MethodCall\AssertIssetToAssertObjectHasPropertyRector;
-
-return RectorConfig::configure()
-    ->withRules([AssertIssetToAssertObjectHasPropertyRector::class]);
