8677f0744de7215f6dd53e4c28721d46f5cbc63d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-26 07:45:20 +0700

    [CodeQuality] Skip loose comparison on assertNotEquals as well on AssertEqualsToSameRector

--- rules/CodeQuality/Rector/MethodCall/AssertEqualsToSameRector.php
+++ rules/CodeQuality/Rector/MethodCall/AssertEqualsToSameRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\PHPUnit\CodeQuality\Rector\MethodCall;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\MethodCall;
