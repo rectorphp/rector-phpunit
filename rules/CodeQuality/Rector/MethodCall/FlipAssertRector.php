a880233a8825d6b66eb81f3f7cd9890fc3739b04
Tomas Votruba <tomas.vot@gmail.com>
2025-01-19 18:38:00 +0000

    add null/false/true support

--- rules/CodeQuality/Rector/MethodCall/FlipAssertRector.php
+++ rules/CodeQuality/Rector/MethodCall/FlipAssertRector.php
@@ -7,6 +7,7 @@ namespace Rector\PHPUnit\CodeQuality\Rector\MethodCall;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Scalar;
@@ -116,6 +117,10 @@ CODE_SAMPLE
     private function isScalarValue(Expr $expr): bool
     {
         if ($expr instanceof Scalar) {
+            return true;
+        }
+
+        if ($expr instanceof ConstFetch) {
             return true;
         }
 
