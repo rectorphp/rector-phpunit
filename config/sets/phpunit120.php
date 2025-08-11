8207d5a1f9dfcb2dd66f4e099ea0bb8b6e9f2578
Matthias Schmidt <matthias@mttsch.dev>
2025-08-11 19:02:56 +0200

    [PHPUnit12] Add `AssertIsTypeMethodCallRector`

--- config/sets/phpunit120.php
+++ config/sets/phpunit120.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\PHPUnit120\Rector\Class_\AssertIsTypeMethodCallRector;
 use Rector\PHPUnit\PHPUnit120\Rector\Class_\RemoveOverrideFinalConstructTestCaseRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(RemoveOverrideFinalConstructTestCaseRector::class);
+    $rectorConfig->rules([RemoveOverrideFinalConstructTestCaseRector::class, AssertIsTypeMethodCallRector::class]);
 };
