57a7e1db9d577f5033f8ac9128e06f084ae948d1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-08-22 13:50:27 +0700

    [Alternative 1] Don't change to assertSame() on numeric string on AssertEqualsToSameRector (#520)
    * [Alternative] Don't change to assertSame() on numeric string
    
    * [Alternative] Don't change to assertSame() on numeric string

--- rules/CodeQuality/Rector/MethodCall/AssertEqualsToSameRector.php
+++ rules/CodeQuality/Rector/MethodCall/AssertEqualsToSameRector.php
@@ -11,6 +11,7 @@ use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Scalar\InterpolatedString;
+use PhpParser\Node\Scalar\String_;
 use PHPStan\Type\BooleanType;
 use PHPStan\Type\Constant\ConstantArrayType;
 use PHPStan\Type\Constant\ConstantBooleanType;
@@ -142,7 +143,11 @@ final class AssertEqualsToSameRector extends AbstractRector
         }
 
         // can happen with magic process
-        return $secondArgType instanceof NeverType;
+        if ($secondArgType instanceof NeverType) {
+            return true;
+        }
+
+        return $args[0]->value instanceof String_ && is_numeric($args[0]->value->value);
     }
 
     private function shouldSkipConstantArrayType(Expr $expr): bool
