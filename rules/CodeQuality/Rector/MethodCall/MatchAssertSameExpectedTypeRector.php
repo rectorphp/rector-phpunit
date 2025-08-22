fbdc97b5621847d6baa967cac7c196a5d2e20505
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-08-22 11:11:25 +0700

    [Alternative 2] Skip assertEquals() on MatchAssertSameExpectedTypeRector

--- rules/CodeQuality/Rector/MethodCall/MatchAssertSameExpectedTypeRector.php
+++ rules/CodeQuality/Rector/MethodCall/MatchAssertSameExpectedTypeRector.php
@@ -82,7 +82,7 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        if (! $this->testsNodeAnalyzer->isPHPUnitMethodCallNames($node, ['assertSame', 'assertEquals'])) {
+        if (! $this->testsNodeAnalyzer->isPHPUnitMethodCallNames($node, ['assertSame'])) {
             return null;
         }
 
