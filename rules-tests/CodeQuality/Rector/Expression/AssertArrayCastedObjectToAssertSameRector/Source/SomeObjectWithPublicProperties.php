00abb0af7a50e7e6b06366457758ba4d906f44fa
Tomas Votruba <tomas.vot@gmail.com>
2025-10-22 13:26:00 +0200

    [code-quality] Add AssertArrayCastedObjectToAssertSameRector

---
+++ rules-tests/CodeQuality/Rector/Expression/AssertArrayCastedObjectToAssertSameRector/Source/SomeObjectWithPublicProperties.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\PHPUnit\Tests\CodeQuality\Rector\Expression\AssertArrayCastedObjectToAssertSameRector\Source;
+
+final class SomeObjectWithPublicProperties
+{
+    public $name;
+
+    public $surname;
+}
