7770d835795fe13553dc68401f3647383e851d8f
Tomas Votruba <tomas.vot@gmail.com>
2025-10-30 12:33:20 +0100

    [code-quality] Add WithCallbackIdenticalToStandaloneAssertsRector (#568)

---
+++ rules/CodeQuality/NodeFactory/FromBinaryAndAssertExpressionsFactory.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\CodeQuality\NodeFactory;
+
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\BinaryOp\Identical;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Instanceof_;
+use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Scalar\Int_;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Expression;
+use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\PhpParser\Node\NodeFactory;
+
+final readonly class FromBinaryAndAssertExpressionsFactory
+{
+    public function __construct(
+        private NodeFactory $nodeFactory,
+        private NodeNameResolver $nodeNameResolver,
+    ) {
+    }
+
+    /**
+     * @param Expr[] $exprs
+     * @return Stmt[]|null
+     */
+    public function create(array $exprs): ?array
+    {
+        $assertMethodCalls = [];
+
+        foreach ($exprs as $expr) {
+            if ($expr instanceof Instanceof_) {
+                if (! $expr->class instanceof Name) {
+                    return null;
+                }
+
+                $className = $expr->class->name;
+                $assertMethodCalls[] = $this->nodeFactory->createMethodCall(
+                    'this',
+                    'assertInstanceOf',
+                    [new ClassConstFetch(new FullyQualified($className), 'class'), $expr->expr]
+                );
+
+                continue;
+            }
+
+            if ($expr instanceof Identical) {
+                if ($expr->left instanceof FuncCall && $this->nodeNameResolver->isName($expr->left, 'count')) {
+                    if ($expr->right instanceof Int_) {
+                        $countedExpr = $expr->left->getArgs()[0]
+                            ->value;
+
+                        // create assertCount()
+                        $assertMethodCalls[] = $this->nodeFactory->createMethodCall(
+                            'this',
+                            'assertCount',
+                            [$expr->right, $countedExpr]
+                        );
+
+                        continue;
+                    }
+
+                    // unclear, fallback to no change
+                    return null;
+                }
+
+                // create assertSame()
+                $assertMethodCalls[] = $this->nodeFactory->createMethodCall(
+                    'this',
+                    'assertSame',
+                    [$expr->right, $expr->left]
+                );
+            } else {
+                // not supported expr
+                return null;
+            }
+        }
+
+        if ($assertMethodCalls === []) {
+            return null;
+        }
+
+        $stmts = [];
+        foreach ($assertMethodCalls as $assertMethodCall) {
+            $stmts[] = new Expression($assertMethodCall);
+        }
+
+        return $stmts;
+    }
+}
---
+++ rules/CodeQuality/ValueObject/ArgAndFunctionLike.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\CodeQuality\ValueObject;
+
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\ArrowFunction;
+use PhpParser\Node\Expr\Closure;
+
+final readonly class ArgAndFunctionLike
+{
+    public function __construct(
+        private Arg $arg,
+        private Closure|ArrowFunction $functionLike
+    ) {
+    }
+
+    public function getArg(): Arg
+    {
+        return $this->arg;
+    }
+
+    public function getFunctionLike(): Closure|ArrowFunction
+    {
+        return $this->functionLike;
+    }
+
+    public function hasParams(): bool
+    {
+        return $this->functionLike->getParams() !== [];
+    }
+}
