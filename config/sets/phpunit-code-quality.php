0b596daeb6bd9a0bfff760405a4a5c08251b640f
Tomas Votruba <tomas.vot@gmail.com>
2025-11-15 22:01:27 +0100

    [depre] Deprecate AssertPropertyExistsRector as method was removed in PHPUnit 10

--- config/sets/phpunit-code-quality.php
+++ config/sets/phpunit-code-quality.php
@@ -29,7 +29,6 @@ use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertFalseStrposToContainsRect
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertInstanceOfComparisonRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertIssetToSpecificMethodRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertNotOperatorRector;
-use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertPropertyExistsRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertRegExpRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertSameBoolNullToSpecificMethodRector;
 use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertSameTrueFalseToAssertTrueFalseRector;
@@ -93,7 +92,6 @@ return static function (RectorConfig $rectorConfig): void {
         AssertFalseStrposToContainsRector::class,
         AssertIssetToSpecificMethodRector::class,
         AssertInstanceOfComparisonRector::class,
-        AssertPropertyExistsRector::class,
         AssertRegExpRector::class,
         AssertFuncCallToPHPUnitAssertRector::class,
         SimplifyForeachInstanceOfRector::class,
