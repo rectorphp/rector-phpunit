265b670b37c6a10ed34d8b730a1d0f1573435e56
Tomas Votruba <tomas.vot@gmail.com>
2026-01-26 12:26:02 +0100

    [PHPUnit 12] Add CreateStubOverCreateMockArgRector (#608)

---
+++ rules-tests/PHPUnit120/Rector/CallLike/CreateStubOverCreateMockArgRector/config/configured_rule.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\PHPUnit120\Rector\CallLike\CreateStubOverCreateMockArgRector;
+
+return RectorConfig::configure()
+    ->withRules([CreateStubOverCreateMockArgRector::class]);
