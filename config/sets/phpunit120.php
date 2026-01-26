35832e5a77dfea15a6e17e7483e9f8a1d223f62d
Tomas Votruba <tomas.vot@gmail.com>
2026-01-26 12:17:11 +0100

    [PHPUnit 12] Add CreateStubOverCreateMockArgRector

--- config/sets/phpunit120.php
+++ config/sets/phpunit120.php
@@ -3,9 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\PHPUnit120\Rector\CallLike\CreateStubOverCreateMockArgRector;
 use Rector\PHPUnit\PHPUnit120\Rector\Class_\AssertIsTypeMethodCallRector;
 use Rector\PHPUnit\PHPUnit120\Rector\Class_\RemoveOverrideFinalConstructTestCaseRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([RemoveOverrideFinalConstructTestCaseRector::class, AssertIsTypeMethodCallRector::class]);
+    $rectorConfig->rules([
+        RemoveOverrideFinalConstructTestCaseRector::class,
+        AssertIsTypeMethodCallRector::class,
+
+        // stubs over mocks
+        CreateStubOverCreateMockArgRector::class,
+    ]);
 };
