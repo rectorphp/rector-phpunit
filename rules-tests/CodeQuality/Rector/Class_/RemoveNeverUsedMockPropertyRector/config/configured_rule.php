c20c0d88efb26081768763b561c424e672d9662f
Tomas Votruba <tomas.vot@gmail.com>
2026-02-04 16:43:13 +0100

    skip if used in array

--- rules-tests/CodeQuality/Rector/Class_/RemoveNeverUsedMockPropertyRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/Class_/RemoveNeverUsedMockPropertyRector/config/configured_rule.php
@@ -5,6 +5,5 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\RemoveNeverUsedMockPropertyRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(RemoveNeverUsedMockPropertyRector::class);
-};
+return RectorConfig::configure()
+    ->withRules([RemoveNeverUsedMockPropertyRector::class]);
