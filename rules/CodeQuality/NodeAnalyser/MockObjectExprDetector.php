095f7ea3e3000783370d3302637fb12e8bc74c4d
GitHub Action <actions@github.com>
2026-04-29 08:25:09 +0000

    [rector] Rector fixes

--- rules/CodeQuality/NodeAnalyser/MockObjectExprDetector.php
+++ rules/CodeQuality/NodeAnalyser/MockObjectExprDetector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\PHPUnit\CodeQuality\NodeAnalyser;
 
-use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\PropertyFetch;
