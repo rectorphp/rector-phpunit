f0d267e7441dc375949fbf479131825c1cc860cc
Michal Kružík <stukov0210@gmail.com>
2026-05-11 16:19:52 +0200

    ReplaceTestAnnotationWithPrefixedFunctionRector does not match @test annotation if it is being part of string (#671)
    * ReplaceTestAnnotationWithPrefixedFunctionRector does not match @test annotation if being part of string
    
    * NewLineSplitter used instead of PHP_EOL explode
    
    * Added break to avoid unnecessary foreach iteration
    
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>
    
    ---------
    
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

--- rules/CodeQuality/Rector/ClassMethod/ReplaceTestAnnotationWithPrefixedFunctionRector.php
+++ rules/CodeQuality/Rector/ClassMethod/ReplaceTestAnnotationWithPrefixedFunctionRector.php
@@ -12,8 +12,10 @@ use Rector\BetterPhpDocParser\PhpDocManipulator\PhpDocTagRemover;
 use Rector\Comments\NodeDocBlock\DocBlockUpdater;
 use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
 use Rector\Rector\AbstractRector;
+use Rector\Util\NewLineSplitter;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use function in_array;
 
 /**
  * @see \Rector\PHPUnit\Tests\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector\ReplaceTestAnnotationWithPrefixedFunctionRectorTest
@@ -84,7 +86,15 @@ CODE_SAMPLE
             return null;
         }
 
-        if (! str_contains($docComment->getText(), '@test')) {
+        $hasAnnotation = false;
+        foreach(NewLineSplitter::split($docComment->getText()) as $row) {
+            if (in_array(trim($row), ['*@test', '* @test'])) {
+                $hasAnnotation = true;
+                break;
+            }
+        }
+
+        if (! $hasAnnotation) {
             return null;
         }
 
