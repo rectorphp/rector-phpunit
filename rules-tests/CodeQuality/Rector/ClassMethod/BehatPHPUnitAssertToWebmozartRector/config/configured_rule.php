191ed98195c62df0fd02755966caff7aa3d49a9d
Tomas Votruba <tomas.vot@gmail.com>
2026-06-28 15:10:54 +0200

    [CodeQuality] Deprecate BehatPHPUnitAssertToWebmozartRector as too narrow (#686)
    Only targets Behat context files - better implemented as a custom rule.
    Marked with DeprecatedInterface, gut logic, drop now-orphaned WebmozartClassName enum and tests.

--- rules-tests/CodeQuality/Rector/ClassMethod/BehatPHPUnitAssertToWebmozartRector/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\BehatPHPUnitAssertToWebmozartRector;
-
-return RectorConfig::configure()
-    ->withRules([BehatPHPUnitAssertToWebmozartRector::class]);
