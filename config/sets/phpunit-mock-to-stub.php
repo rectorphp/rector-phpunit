a741141ad0ff32dcde8ea761e09b233ecb964686
Tomas Votruba <tomas.vot@gmail.com>
2026-07-11 11:46:12 +0200

    [CodeQuality] Add BareCreateMockAssignToDirectUseRector to phpunit-mock-to-stub set (#708)

--- config/sets/phpunit-mock-to-stub.php
+++ config/sets/phpunit-mock-to-stub.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\AddIntersectionVarToMockObjectPropertyRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\BareCreateMockAssignToDirectUseRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\AddStubIntersectionVarToStubPropertyRector;
 use Rector\PHPUnit\PHPUnit120\Rector\CallLike\CreateStubInCoalesceArgRector;
 use Rector\PHPUnit\PHPUnit120\Rector\CallLike\CreateStubOverCreateMockArgRector;
@@ -21,5 +22,6 @@ return static function (RectorConfig $rectorConfig): void {
         MockObjectVarToStubRector::class,
         AddIntersectionVarToMockObjectPropertyRector::class,
         AddStubIntersectionVarToStubPropertyRector::class,
+        BareCreateMockAssignToDirectUseRector::class,
     ]);
 };
