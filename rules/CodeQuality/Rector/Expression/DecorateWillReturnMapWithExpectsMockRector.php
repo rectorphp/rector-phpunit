b9def97b358b307339187c8540977344d6ce197a
Tomas Votruba <tomas.vot@gmail.com>
2026-07-13 11:16:46 +0200

    [CodeQuality] Use atLeast() instead of exactly() in DecorateWillReturnMapWithExpectsMockRector

--- rules/CodeQuality/Rector/Expression/DecorateWillReturnMapWithExpectsMockRector.php
+++ rules/CodeQuality/Rector/Expression/DecorateWillReturnMapWithExpectsMockRector.php
@@ -70,7 +70,7 @@ final class SomeTest extends TestCase
     {
         $this->someMock = $this->createMock(SomeClass::class);
 
-        $this->someMock->expects($this->exactly(2))
+        $this->someMock->expects($this->atLeast(2))
             ->method("someMethod")
             ->willReturnMap([
                 ["arg1", "arg2", "result1"],
@@ -166,7 +166,7 @@ CODE_SAMPLE
             new Identifier('expects'),
             [new Arg(new MethodCall(
                 new Variable('this'),
-                new Identifier('exactly'),
+                new Identifier('atLeast'),
                 [new Arg(new Int_($mapCount))]
             ))]
         );
