7787aa14249736a30bb103b3818c4310ba9005fd
Tomas Votruba <tomas.vot@gmail.com>
2026-07-31 20:54:51 +0200

    Decorate annotation to attribute rules with composer constraint, register them in composer-based set

--- config/sets/composer-based.php
+++ config/sets/composer-based.php
@@ -6,6 +6,12 @@ use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
 use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\AnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\RequiresAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\TicketAnnotationToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DependsAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\TestWithAnnotationToAttributeRector;
 use Rector\PHPUnit\ValueObject\AnnotationWithValueToAttribute;
 
 /**
@@ -13,6 +19,17 @@ use Rector\PHPUnit\ValueObject\AnnotationWithValueToAttribute;
  * as not every attribute exists in every PHPUnit version.
  */
 return static function (RectorConfig $rectorConfig): void {
+    // each of these rules declares the "phpunit/phpunit" version its attributes were added in,
+    // @see ComposerPackageConstraintInterface
+    $rectorConfig->rules([
+        TicketAnnotationToAttributeRector::class,
+        TestWithAnnotationToAttributeRector::class,
+        DataProviderAnnotationToAttributeRector::class,
+        CoversAnnotationWithValueToAttributeRector::class,
+        RequiresAnnotationWithValueToAttributeRector::class,
+        DependsAnnotationWithValueToAttributeRector::class,
+    ]);
+
     // both attributes were added in PHPUnit 10.0
     $rectorConfig->ruleWithConfigurationComposerVersionBound(AnnotationWithValueToAttributeRector::class, [
         // the PHPUnit 10 spelling of the "backupStaticAttributes" annotation
