69ce6b24d2c431f2ce5309fabf71848417b61020
Tomas Votruba <tomas.vot@gmail.com>
2026-09-18 00:39:56 +0200

    Lower duplicated-code --min-tokens to 100 (#786)

--- rules/CodeQuality/Rector/Class_/VoidMethodWithCallbackToWillReturnCallbackRector.php
+++ rules/CodeQuality/Rector/Class_/VoidMethodWithCallbackToWillReturnCallbackRector.php
@@ -11,28 +11,22 @@ use PhpParser\Node\Expr\ArrowFunction;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\Throw_;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Scalar;
-use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\NodeFinder;
 use PHPStan\Reflection\ClassReflection;
-use PHPStan\Type\IntersectionType;
-use PHPStan\Type\NeverType;
-use PHPStan\Type\ObjectType;
-use PHPStan\Type\Type;
 use PHPStan\Type\VoidType;
-use Rector\Enum\ClassName;
+use Rector\PHPUnit\CodeQuality\NodeAnalyser\MockedMethodTypeResolver;
 use Rector\PHPUnit\CodeQuality\NodeAnalyser\SetUpAssignedMockTypesResolver;
 use Rector\PHPUnit\CodeQuality\Reflection\MethodParametersAndReturnTypesResolver;
+use Rector\PHPUnit\CodeQuality\ValueObject\MockedMethod;
 use Rector\PHPUnit\CodeQuality\ValueObject\ParamTypesAndReturnType;
-use Rector\PHPUnit\Enum\PHPUnitClassName;
 use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
 use Rector\Rector\AbstractRector;
 use Rector\Reflection\ReflectionResolver;
@@ -49,6 +43,7 @@ final class VoidMethodWithCallbackToWillReturnCallbackRector extends AbstractRec
         private readonly SetUpAssignedMockTypesResolver $setUpAssignedMockTypesResolver,
         private readonly MethodParametersAndReturnTypesResolver $methodParametersAndReturnTypesResolver,
         private readonly ReflectionResolver $reflectionResolver,
+        private readonly MockedMethodTypeResolver $mockedMethodTypeResolver,
     ) {
     }
 
@@ -221,44 +216,14 @@ CODE_SAMPLE
             return false;
         }
 
-        $parentMethodCall = $methodCall->var;
-        if (! $this->isName($parentMethodCall->name, 'method')) {
-            return false;
-        }
-
-        $methodNameExpr = $parentMethodCall->getArgs()[0]
-            ->value;
-        if (! $methodNameExpr instanceof String_) {
-            return false;
-        }
-
-        $methodName = $methodNameExpr->value;
-        $callerType = $this->getType($parentMethodCall->var);
-
-        if ($callerType instanceof ObjectType && in_array(
-            $callerType->getClassName(),
-            [
-                PHPUnitClassName::INVOCATION_MOCKER,
-                PHPUnitClassName::INVOCATION_MOCKER_INTERFACE,
-                PHPUnitClassName::INVOCATION_STUBBER,
-            ],
-            true
-        )) {
-            $parentMethodCall = $parentMethodCall->var;
-
-            if ($parentMethodCall instanceof MethodCall) {
-                $callerType = $this->getType($parentMethodCall->var);
-            }
-        }
-
-        $callerType = $this->fallbackMockedObjectInSetUp($callerType, $parentMethodCall, $propertyNameToMockedTypes);
-        if (! $callerType instanceof IntersectionType) {
+        $mockedMethod = $this->mockedMethodTypeResolver->resolve($methodCall->var, $propertyNameToMockedTypes);
+        if (! $mockedMethod instanceof MockedMethod) {
             return false;
         }
 
         $paramTypesAndReturnType = $this->methodParametersAndReturnTypesResolver->resolveFromReflection(
-            $callerType,
-            $methodName,
+            $mockedMethod->getCallerType(),
+            $mockedMethod->getMethodName(),
             $currentClassReflection
         );
 
@@ -341,45 +306,5 @@ CODE_SAMPLE
     private function isPureValue(Expr $expr): bool
     {
         return $expr instanceof Scalar || $expr instanceof ConstFetch || $expr instanceof Variable;
-    }
-
-    /**
-     * @param array<string, string> $propertyNameToMockedTypes
-     */
-    private function fallbackMockedObjectInSetUp(
-        Type $callerType,
-        Expr $expr,
-        array $propertyNameToMockedTypes
-    ): Type {
-        if (! $callerType instanceof ObjectType && ! $callerType instanceof NeverType) {
-            return $callerType;
-        }
-
-        if (! $expr instanceof MethodCall) {
-            return $callerType;
-        }
-
-        if ($callerType instanceof ObjectType && $callerType->getClassName() !== ClassName::MOCK_OBJECT) {
-            return $callerType;
-        }
-
-        // type is missing, because of "final" keyword on mocked class
-        // resolve from setUp assignment instead
-        if (! $expr->var instanceof PropertyFetch && ! $expr->var instanceof Variable) {
-            return $callerType;
-        }
-
-        if ($expr->var instanceof Variable) {
-            $propertyOrVariableName = $this->getName($expr->var);
-        } else {
-            $propertyOrVariableName = $this->getName($expr->var->name);
-        }
-
-        if (isset($propertyNameToMockedTypes[$propertyOrVariableName])) {
-            $mockedType = $propertyNameToMockedTypes[$propertyOrVariableName];
-            return new IntersectionType([$callerType, new ObjectType($mockedType)]);
-        }
-
-        return $callerType;
     }
 }
